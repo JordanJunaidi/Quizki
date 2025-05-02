b422a69fb47a6a7572570ba9fbb502e503fa759a
Jordan Junaidi <64229981+JordanJunaidi@users.noreply.github.com>
2025-05-02 16:11:15 -0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,27 +20,27 @@ export const Home = () => {
 
       <div className="smiski-preview">
         <div>
-          <img src="/public/sunday_garden.png" />
+          <img src="/sunday_garden.png" />
           <p>Smiski Gardening</p>
         </div>
 
         <div>
-          <img src="/public/exercise_dumbell.png" />
+          <img src="/exercise_dumbell.png" />
           <p>Smiski Dumbell</p>
         </div>
 
         <div>
-          <img src="/public/bath_duck.png" />
+          <img src="/bath_duck.png" />
           <p>Smiski With Duck</p>
         </div>
 
         <div>
-          <img src="/public/toilet_rest.png" />
+          <img src="/toilet_rest.png" />
           <p>Smiski Resting</p>
         </div>
 
         <div>
-          <img src="/public/work_idea.png" />
+          <img src="/work_idea.png" />
           <p>Smiski Good Idea</p>
         </div>
       </div>
